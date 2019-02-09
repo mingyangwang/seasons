ce2371454b9210597c41eca3a0815188a4cb283f
王铭阳 <wangmingyang0111@gmail.com>
2019-02-09 19:59:02 +0200

    use if

--- src/index.js
+++ src/index.js
@@ -6,20 +6,30 @@ class App extends React.Component {
 		super(props);
 		//THIS IS THE ONLY TIME WE DO DIREXT ASSIGNMENT
 		//TO this.state
-		this.state = { lat: null };
+		this.state = { lat: null, errorMessage: "" };
 
 		window.navigator.geolocation.getCurrentPosition(
 			position => {
 				//We called setstate!!!
 				this.setState({ lat: position.coords.latitude });
 			},
-			err => console.log(err)
+			err => {
+				this.setState({ errorMessage: err.message });
+			}
 		);
 	}
 
 	//React says we have to define render!!
 	render() {
-		return <div>Latitude:{this.state.lat}</div>;
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error:{this.state.errorMessage}</div>;
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <div>Latitude:{this.state.lat}</div>;
+		}
+
+		return <div>Loading!</div>;
 	}
 }
 
